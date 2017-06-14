965e9e73290ed82f0b2293fec287dfddeb9761e5
Sampaleen <spaleen@udel.edu>
2017-06-14 11:37:13 -0400

    nav bar update

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+
 import Full_logo from '../assets/full_logo.png';
 import Small_logo from '../assets/small_logo.png';
 import styles from '../style/Header.css';
@@ -12,9 +19,28 @@ class Header extends Component {
   render() {
 
     return (
-      <div className = "header">
-        <img src = {Full_logo} className = "image" id = "large"/>
-        <img src = {Small_logo} className = "image" id = "small" />
+      <div>
+        <div className = "header">
+          <img src = {Full_logo} className = "image" id = "large"/>
+          <img src = {Small_logo} className = "image" id = "small" />
+        </div>
+        <nav className="navbar navbar-default">
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton label="Home" className="menuBarButton"/>
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton label="Announcements" className="menuBarButton"/>
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton label="Projects" className="menuBarButton"/>
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton label="Contact" className="menuBarButton"/>
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton label="Login" id = "login" className="menuBarButton"/>
+          </MuiThemeProvider>
+        </nav>
       </div>
     );
   }
