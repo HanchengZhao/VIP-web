2cffb0613a7f0cedae02c6eae50c98d5e5789740
Henry-Z <chancidy@gmail.com>
2017-06-15 12:01:07 -0400

    add router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>      
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
