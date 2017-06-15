458ac8b5962b7424bab0b0a74fa1ccf01bd3050a
poiuyyy <poiuyyy@udel.edu>
2017-06-15 15:03:20 -0400

    path change

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import {
     BrowserRouter as Router,
     Route,
     Link
-} from 'react-router-dom'
+
+} from 'react-router-dom';
+
+
 
 import Announcement from './component/Announcement';
 import DropDown from './component/DropDown';
-import quiz from './quiz';
 import Header from './component/Header';
 import Footer from './component/Footer';
 
+import InstructionComponent from './component/InstructionComponent';
+import ApplyFormComponent from './component/ApplyFormComponent';
+import UploadCsvComponent from './component/UploadCsvComponent';
+import EditableGridComponent from './component/EditableGridComponent';
 import './style/App.css';
+import * as firebase from 'firebase';
+  var config = {
+    apiKey: "AIzaSyBzy4ctl-AgyeZSu2Mu9AUuVSHiZh0-TSg",
+    authDomain: "react-native-7f0db.firebaseapp.com",
+    databaseURL: "https://react-native-7f0db.firebaseio.com",
+    projectId: "react-native-7f0db",
+    storageBucket: "react-native-7f0db.appspot.com",
+    messagingSenderId: "832639299928"
+  };
+  firebase.initializeApp(config);
+
+
 
 injectTapEventPlugin();
 
@@ -33,16 +51,27 @@ const Contact = () => (
     <h2>Contact</h2>
   </div>
 )
+const Test = () => (
+  <div>
+    <h2>Contact</h2>
+    <InstructionComponent />
+    <ApplyFormComponent />
+    <UploadCsvComponent  />
+    <EditableGridComponent />
+  </div>
+)
 class App extends Component {
 
   constructor(){
     super();
   }
   componentDidMount(){
- 
+
   }
+
   render() {
     return (
+
       <div>
         <Header />
           <div className="App">
@@ -50,6 +79,7 @@ class App extends Component {
             <Route path="/announcement" component={Announcement}/>
             <Route path="/projects" component={Projects}/>
             <Route path="/contact" component={Contact}/>
+            <Route path="/test" component={Test}/>
           </div>
         <Footer />
       </div>
