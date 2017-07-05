5b7018160e1314bebc7b70c94705b937759d6ab9
Henry-Z <chancidy@gmail.com>
2017-07-05 16:18:11 -0400

    change projectApplication tool to AdminRouter

--- src/component/Admin/AdminPage.js
+++ src/component/Admin/AdminPage.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import MuiButton from '../MuiButton';
 
+import userStore from '../../stores/UserStore';
 import Tool from './TeamApplication/ProjectApplicationTool'
 
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {AdminRoute} from '../Route';
 
 
 class AdminPage extends Component {
   render() {
     return(
       <div>
-        <h1>Admin Page</h1>
-        <Route path = "/admin/projectApplication" component={Tool} />
+        <h1>Admin Page </h1>
+        <AdminRoute user={userStore} path = "/admin/projectApplication" component={Tool} />
         <div style = {{paddingTop:"20px"}}>
           <Link to="/admin/projectApplication/pending"><MuiButton label = "Project Tool" color = "#8c1d40"/></Link>
         </div>
