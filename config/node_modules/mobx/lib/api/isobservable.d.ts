42963de47b15a963187d1ba1ac58ce58a3006c21
Henry-Z <chancidy@gmail.com>
2017-07-05 13:54:58 -0400

    removed firebase && node modules

--- config/node_modules/mobx/lib/api/isobservable.d.ts
+++
@@ -1,6 +0,0 @@
-/**
-    * Returns true if the provided value is reactive.
-    * @param value object, function or array
-    * @param propertyName if propertyName is specified, checkes whether value.propertyName is reactive.
-    */
-export declare function isObservable(value: any, property?: string): boolean;
