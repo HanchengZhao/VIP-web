4f6a607d4be840acad154818df71492f4232f21d
Henry-Z <chancidy@gmail.com>
2017-07-22 20:43:36 -0400

    implemented local image uploading

--- src/component/announcements/AnnouncementCreate.js
+++ src/component/announcements/AnnouncementCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import Dropzone from 'react-dropzone';
 import DatePicker from 'material-ui/DatePicker';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -29,14 +30,23 @@ const styles = {
   floatingLabelStyle: {
     color: grey500,
   },
-  datePicker: getMuiTheme({
-    datePicker: {
-      selectColor: "#ffc425"
-    },
-    flatButton: {
-      primaryTextColor: "#ffc425"
-    }
-  })
+  datePicker: {
+    theme: getMuiTheme({
+      datePicker: {
+        selectColor: "#ffc425"
+      },
+      flatButton: {
+        primaryTextColor: "#ffc425"
+      }
+    }),
+    position: "col-md-3"
+  },
+  dropZone: {
+    position: "col-md-3 pull-right"
+  },
+  publishButton: {
+    marginTop: "20px"
+  }
 };
 const rawPath = "Announcement_Raw_Data"
 const announcementPath = "Announcement/admin"
@@ -52,12 +62,14 @@ class AnnouncementCreate extends Component {
       endDate: "",
       redirectToAnnouncement: false
     }
+    this.debounceContent = this.debounceContent.bind(this);
     this.titleChange = this.titleChange.bind(this);
     this.contentChange = this.contentChange.bind(this);
     this.publish = this.publish.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.handleChangeStartDate = this.handleChangeStartDate.bind(this);
     this.handleChangeEndDate = this.handleChangeEndDate.bind(this);
+    this.onDrop = this.onDrop.bind(this);
   }
   debounceTitle = debounce(300, (e) => {
     this.setState({
@@ -76,8 +88,11 @@ class AnnouncementCreate extends Component {
   }
 
   contentChange(e) {
-    e.persist()
-    this.debounceContent(e)
+    this.setState({
+      content: e.target.value
+    })
+    // e.persist()
+    // this.debounceContent(e)
   }
 
   publish(){
@@ -121,12 +136,27 @@ class AnnouncementCreate extends Component {
     });
   };
 
-  handleChangeEndDate = (event, date) => {
+  handleChangeEndDate(event, date){
     this.setState({
       endDate: date,
     });
   };
 
+  onDrop(photos){
+    let dt = new Date();
+    let newdt = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();// yy/mm/dd
+    const ref = firebase.storage().ref()
+    photos.forEach((photo) => {
+      console.log(photo);
+      let photoRef = ref.child("announcement_photos/" + newdt + "/" + photo.name);
+      photoRef.put(photo)
+      .then((snap) => {
+        this.setState(prevState => ({
+          content : prevState.content + `\n\n<img alt="${photo.name}" src="${snap.downloadURL}" width="50%">`
+        }))
+      })
+    })
+  }
 
   render(){
     const actions = [
@@ -138,7 +168,7 @@ class AnnouncementCreate extends Component {
     ];
     return (
       <div>
-        <div className="clearfix" style={{marginBottom: "10px"}}>
+        <div  style={{marginBottom: "10px"}}>
           <MuiThemeProvider>
             <div className="panel panel-default">
               <div className="panel-heading">
@@ -162,30 +192,56 @@ class AnnouncementCreate extends Component {
                 underlineFocusStyle={styles.underlineStyle}
                 floatingLabelStyle={styles.floatingLabelStyle}
                 fullWidth={true}
+                value={this.state.content}
               />
               </div>
               
             </div>
           </MuiThemeProvider>
-          <MuiThemeProvider muiTheme={styles.datePicker}>
-            <div>
-              <DatePicker
-                onChange={this.handleChangeStartDate}
-                floatingLabelText="Start Date"
-                defaultDate={this.state.startDate}
-                container="inline"
-              />
-              <DatePicker
-                onChange={this.handleChangeEndDate}
-                floatingLabelText="End Date"
-                container="inline"
-                shouldDisableDate={this.disablePrevDates(this.state.startDate)}
-              />
+          <div className="row">
+            <div className={styles.datePicker.position}>
+            <MuiThemeProvider muiTheme={styles.datePicker.theme}>
+              <div >
+                <DatePicker
+                  onChange={this.handleChangeStartDate}
+                  floatingLabelText="Start Date"
+                  defaultDate={this.state.startDate}
+                  container="inline"
+                />
+                <DatePicker
+                  onChange={this.handleChangeEndDate}
+                  floatingLabelText="End Date"
+                  container="inline"
+                  shouldDisableDate={this.disablePrevDates(this.state.startDate)}
+                />
+              </div>
+            </MuiThemeProvider>
             </div>
-          </MuiThemeProvider>
-          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-            <RaisedButton className="pull-right" label = "Publish"  backgroundColor = "#ffc425" onClick={this.publish} />
-          </MuiThemeProvider>
+            <div className={styles.dropZone.position}>
+              <Dropzone
+                accept="image/jpeg, image/png, image/gif"
+                onDrop={this.onDrop}
+              >
+                {({ isDragActive, isDragReject, acceptedFiles, rejectedFiles }) => {
+                  if (isDragActive) {
+                    return "This file type is valid";
+                  }
+                  if (isDragReject) {
+                    return "This file type is not valid";
+                  }
+                  return acceptedFiles.length || rejectedFiles.length
+                    ? `Accepted ${acceptedFiles.length}, rejected ${rejectedFiles.length} files`
+                    : "Upload local images via drop/click.";
+                }}
+              </Dropzone>
+            </div>
+          </div>
+          <div className="row" style={styles.publishButton}>
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+              <RaisedButton className="pull-right" label = "Publish"  backgroundColor = "#ffc425" onClick={this.publish} />
+            </MuiThemeProvider>
+          </div>
+          
         </div>
         {/* <br /> */}
         <h4>Preview:</h4>
