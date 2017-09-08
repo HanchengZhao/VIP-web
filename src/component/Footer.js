408c6d08dfa56b57b542ade8ed1cfaa510532db2
vaishnaviraj63 <vaishnaviraj63@gmail.com>
2017-09-07 18:29:21 -0700

    Styling chnages

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import Small_logo from '../assets/small_logo.png';
-import '../style/Footer.css';
+import '../style/Header.css';
 
 class Footer extends Component {
 
   render() {
     return (
-      <div className = 'footer'>
-          <img src = {Small_logo} alt = "ASU footer" className = "image-footer" />
+      <div className="footer">
+        <div id="innovation-footer">
+          <div id="innovation-bar">
+            <div className="innovation-top">
+              <div className="innovation-status">
+                <a href="https://yourfuture.asu.edu/rankings"><span>ASU is #1 in the U.S. for Innovation</span></a>
+              </div>
+              <div className="innovation-hidden">
+                <a href="https://yourfuture.asu.edu/rankings"><img src="//www.asu.edu/asuthemes/4.6/assets/best-college-2017.png" alt="Best Colleges U.S. News Most Innovative 2017"/></a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-menu">
+            <ul className="default">
+              <li><a href="http://www.asu.edu/copyright/">Copyright &amp; Trademark</a></li>
+              <li><a href="http://www.asu.edu/accessibility/">Accessibility</a></li>
+              <li><a href="http://www.asu.edu/privacy/">Privacy</a></li>
+              <li><a href="http://www.asu.edu/asujobs">Jobs</a></li>
+              <li><a href="http://www.asu.edu/emergency/">Emergency</a></li>
+              <li className="no-border"><a href="http://www.asu.edu/contactasu/">Contact ASU</a></li>
+          </ul>
+        </div>
+      </div>
       </div>
     );
   }
--- src/component/Header.js
+++ src/component/Header.js
@@ -14,7 +14,18 @@ import Small_logo from '../assets/small_logo.png';
 import Vip_logo from '../assets/Vip_logo.png';
 import '../style/Header.css';
 
-
+const style = {
+    projectTitle: {
+      color: "#8C1D40",
+      align:"center"
+    },
+    heading:{
+      color: "#FFC627"
+    }, 
+    head:{
+        paddingLeft: "10px"
+    } 
+  }
 
 class Header extends Component {
 
@@ -22,39 +33,122 @@ class Header extends Component {
 
     return (
       <div>
-        <div className = "header">
-          <a href = "https://www.asu.edu/">
-            <img src = {Full_logo}  className = "image" id = "large"/>
-            <img src = {Small_logo} className = "image" id = "small" />
-          </a>
-            <img src = {Vip_logo} className = "image" id = "VIP" />
+        <div>
+            <div id="main-search" className="main-search closed"></div>
+            <div id="asu_hdr" className="asu_hdr_white">
+                <div id="asu_mobile_hdr">
+                    <div id="asu_logo">
+                        <a href="//www.asu.edu/" target="_top" title="Arizona State University">
+                        <img alt="ASU Logo" src="//www.asu.edu/asuthemes/4.6/assets/full_logo.png"/></a>
+                    </div>
+                    <div className="asuhide f-navicon" id="asu_mobile_button" onclick="javascript:ASUHeader.toggleASU();">
+                        <a href="javascript:ASUHeader.toggleASU();">Menu</a>
+                    </div>
+                    <div className="f-search" id="search_new" onclick="javascript:ASUHeader.toggleASU('main-search');"></div>
+                </div>
+                <div className="closed" id="asu_mobile_menu">
+                    <div id="asu_nav_wrapper">
+                        <ul id="asu_login_module">
+                            <li className="end" id="asu_hdr_ssi">
+                                <a href="//weblogin.asu.edu/cgi-bin/login" onclick="this.href=ASUHeader.alterLoginHref(this.href);" onfocus="this.href=ASUHeader.alterLoginHref(this.href);" onmouseover="this.href=ASUHeader.alterLoginHref(this.href);" target="_top">Sign In</a>
+                            </li>
+                        </ul>
+                        <div id="asu_nav_menu" role="navigation" aria-label="ASU">
+                            <div id="asu_universal_nav">
+                                <ul>
+                                    <li className="parent"><a href="//www.asu.edu/" target="_top">ASU Home</a>
+                                        <ul>
+                                            <li><a className="first" href="//www.asu.edu/?feature=newsevents" target="_top" title="News and Events">News/Events</a></li>
+                                            <li><a href="//www.asu.edu/?feature=academics" target="_top" title="Academics">Academics</a></li>
+                                            <li><a href="//www.asu.edu/?feature=research" target="_top" title="Research">Research</a></li>
+                                            <li><a href="//www.asu.edu/?feature=athletics" target="_top" title="Athletics">Athletics</a></li>
+                                            <li><a href="//www.asu.edu/?feature=alumni" target="_top" title="Alumni">Alumni</a></li>
+                                            <li><a href="//www.asu.edu/?feature=giving" target="_top" title="Giving">Giving</a></li>
+                                            <li><a href="//www.asu.edu/?feature=president" target="_top" title="President">President</a></li>
+                                            <li><a href="//www.asu.edu/?feature=aboutasu" target="_top" title="About ASU">About ASU</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="//my.asu.edu/" target="_top">My ASU</a></li>
+                                    <li className="parent"><a href="//www.asu.edu/colleges/" target="_top">Colleges &amp; Schools</a>
+                                        <ul>
+                                            <li><a className="first" href="//artsandsciences.asu.edu/" target="_top" title="Arts and Sciences website">Arts and Sciences</a></li>
+                                            <li><a href="//wpcarey.asu.edu/" target="_top" title="W. P. Carey School of Business Web and Morrison School of Agribusiness website">Business</a></li>
+                                            <li><a href="//herbergerinstitute.asu.edu" target="_top" title="Herberger Institute for Design and the Arts website">Design and the Arts</a></li>
+                                            <li><a href="//education.asu.edu/" target="_top" title="Mary Lou Fulton Teachers College website">Education</a></li>
+                                            <li><a href="//engineering.asu.edu/" target="_top" title="Engineering website">Engineering</a></li>
+                                            <li><a href="//sfis.asu.edu/" target="_top" title="Future of Innovation in Society website">Future of Innovation in Society</a></li>
+                                            <li><a href="//graduate.asu.edu" target="_top" title="Graduate College website">Graduate</a></li>
+                                            <li><a href="//chs.asu.edu/" target="_top" title="Health Solutions website">Health Solutions</a></li>
+                                            <li><a href="//honors.asu.edu/" target="_top" title="Barrett, The Honors College website">Honors</a></li>
+                                            <li><a href="//cronkite.asu.edu" target="_top" title="Walter Cronkite School of Journalism and Mass Communication website">Journalism</a></li>
+                                            <li><a href="//www.law.asu.edu/" target="_top" title="Sandra Day O' Connor College of Law website">Law</a></li>
+                                            <li><a href="//nursingandhealth.asu.edu/" target="_top" title="College of Nursing and Health Innovation website">Nursing and Health Innovation</a></li>
+                                            <li><a href="//copp.asu.edu" target="_top" title="College of Public Programs website">Public Service and Community Solutions</a></li>
+                                            <li><a href="//schoolofsustainability.asu.edu" target="_top" title="School of Sustainability website">Sustainability</a></li>
+                                            <li><a href="//uc.asu.edu/" target="_top" title="University College website">University College</a></li>
+				                            <li><a target="_top" href="http://www.thunderbird.edu/" title="Thunderbird School of Global Management website">Thunderbird School of Global Management</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="parent"><a href="//www.asu.edu/map/" target="_top">Map &amp; Locations</a>
+                                        <ul>
+                                            <li><a className="border first" href="//www.asu.edu/map/" target="_top">Map</a></li>
+                                            <li><a href="//campus.asu.edu/tempe/" target="_top" title="Tempe campus">Tempe</a></li>
+                                            <li><a href="//campus.asu.edu/west/" target="_top" title="West campus">West</a></li>
+                                            <li><a href="//campus.asu.edu/polytechnic/" target="_top" title="Polytechnic campus">Polytechnic</a></li>
+                                            <li><a href="//campus.asu.edu/downtown/" target="_top" title="Downtown Phoenix campus">Downtown Phoenix</a></li>
+                                            <li><a href="//asuonline.asu.edu/" target="_top" title="Online and Extended campus">Online and Extended</a></li>
+                                            <li><a href="//havasu.asu.edu/" target="_top">Lake Havasu</a></li>
+                            				<li><a class="border" target="_top" href="http://www.thunderbird.edu/about-thunderbird/locations/phoenix-arizona">Thunderbird</a></li>
+                                            <li><a href="//skysong.asu.edu/" target="_top">Skysong</a></li>
+                                            <li><a href="//asuresearchpark.com/" target="_top">Research Park</a></li>
+                                            <li><a href="//washingtoncenter.asu.edu/" target="_top">Washington D.C.</a></li>
+                                            <li><a href="//wpcarey.asu.edu/mba/china-program/english/" target="_top">China</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="//isearch.asu.edu/" target="_top" title="Directory, Index and other info">Directory</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="asu_search">
+                        <div id="asu_search_module">
+                            <form target="_top" action="https://search.asu.edu/search" id="asu_search_placeholder" method="get" name="gs" role="search">
+                                <label for="masu_search_box">Search</label>
+                                <input className="asu_search_button" type="submit" value="Search"/>
+                                <input name="q" class="asu_search_box" id="masu_search_box" placeholder="Search" autocomplete="off" type="text"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <nav className="navbar navbar-default navbar-static-top" id="TEST" >
+        <h3 style={style.head}>Vertically Integrated Projects - VIP</h3>
 
+        <nav className="navbar">
             <button className="navbar-toggle" data-toggle = "collapse" data-target=".navHeaderCollapse">
               <span className = "icon-bar" />
               <span className = "icon-bar" />
               <span className = "icon-bar" />
               <span className = "icon-bar" />
             </button>
-
-          <div className="collapse navbar-collapse navHeaderCollapse" >
+            <div className="collapse navbar-collapse navHeaderCollapse" >
               <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
                 <div>
                   <div data-toggle="collapse" data-target=".navHeaderCollapse" className="visible-xs row" > 
-                    <Link to="/"><FlatButton label="Home" className="menuBarButton" /></Link>
-                    <Link to="/announcement"><FlatButton label="Announcements" className="menuBarButton"/></Link>
-                    <Link to="/projects"><FlatButton label="Projects" className="menuBarButton"/></Link>
-                    <Link to="/contact"><FlatButton label="Contact" className="menuBarButton"/></Link>
-                    <Login user={this.props.user} />
+                    <Link to="/"><FlatButton label="Home" className="menuBarButton" fullWidth={true}/></Link>
+                    <Link to="/projects"><FlatButton label="Projects" className="menuBarButton" fullWidth={true}/></Link>
+                    <Link to="/faculty"><FlatButton label="Faculty" className="menuBarButton" fullWidth={true}/></Link>
+                    <Link to="/dashboard"><FlatButton label="Dashboard" className="menuBarButton" fullWidth = {true}/></Link>
+                    <LoginAvatar user={this.props.user}/>
+                    <Login user={this.props.user} Width = {true}/>
                   </div>
                   <div className="hidden-xs"> 
-                    <Link to="/"><FlatButton label="Home" className="menuBarButton" /></Link>
-                    <Link to="/announcement"><FlatButton label="Announcements" className="menuBarButton"/></Link>
-                    <Link to="/projects"><FlatButton label="Projects" className="menuBarButton"/></Link>
-                    <Link to="/contact"><FlatButton label="Contact" className="menuBarButton"/></Link>
+                    <Link to="/"><FlatButton label="Home" className="menuBarButton"/></Link>
+                    <Link to="/projects"><FlatButton label="For Students" className="menuBarButton"/></Link>
+                    <Link to="/faculty"><FlatButton label="For Faculty" className="menuBarButton"/></Link>
                     <Login user={this.props.user} />
-                    <LoginAvatar user={this.props.user}/>                                        
+                    <LoginAvatar user={this.props.user}/>
+                    <Link to="/dashboard"><FlatButton label="Dashboard" className="menuBarButton" id = "dashboard" /></Link>                                       
                   </div>
                 </div>  
               </MuiThemeProvider>
