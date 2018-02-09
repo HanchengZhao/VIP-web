25f786978b0e60ced944c39d822618a073a05089
Sampaleen <spaleen@udel.edu>
2018-02-09 11:19:26 -0500

    Style Issues

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -3,6 +3,9 @@ import ProjectList from '../projects/ProjectList';
 import AnnouncementList from '../announcements/AnnouncementList';
 import {Secondary} from '../../Theme';
 
+import Card from 'material-ui/Card';
+import logo from '../../assets/Vip_logo.png';
+
 const style = {
   hr: {
     borderTop: "3px double #8c8b8b"
@@ -13,27 +16,37 @@ const style = {
   },
   cardHeader: {
     textAlign : 'left',
-    fontSize: '1.3em',
-    color: "#8C1D40"
+    color: "#8C1D40",
+    textAlign:'center',
+    clear:'left'
   },
+  font:{
+    color:"#212121",
+    clear:'left'
+  },
+  image:{
+    float:'left',
+    height:'70px',
+  }
 }
 class Home extends Component {
   render() {
     return(
       <div>
-        <p style={style.card}>
-          <p style={style.cardHeader}><b>What is VIP?</b></p>
-          <p>VIP at ASU capitalizes on the interests of undergraduates to engage in ongoing scientific research. Beginning as freshmen or sophomores, teams collaborate with upperclassmen and graduate students to address real problems guided by faculty scientists. Students earn academic or honors credits over 4 years.</p>
-          <p>VIP experience:
+        <Card style={style.card} zDepth = {0}>
+        <img src = {logo} style = {style.image}/>
+        <h1 style={style.cardHeader}>What is VIP?</h1>
+          <p style = {style.font}>VIP at ASU capitalizes on the interests of undergraduates to engage in ongoing scientific research. Beginning as freshmen or sophomores, teams collaborate with upperclassmen and graduate students to address real problems guided by faculty scientists. Students earn academic or honors credits over 4 years.</p>
+          <p style = {style.font}>VIP experience:
             <ul>
-            <li>Prepares students for the workplace or more advanced study</li>
-            <li>Facilitates in-depth experience and applied learning through multidisciplinary challenges</li>
-            <li>Enables broad understanding of the innovation process, and</li>
-            <li>Allows for mastery of critical thinking skills and exposure to a variety of skills and roles.</li>
+            <li style = {style.font}>Prepares students for the workplace or more advanced study</li>
+            <li style = {style.font}>Facilitates in-depth experience and applied learning through multidisciplinary challenges</li>
+            <li style = {style.font}>Enables broad understanding of the innovation process, and</li>
+            <li style = {style.font}>Allows for mastery of critical thinking skills and exposure to a variety of skills and roles.</li>
           </ul>
           </p>
-          <p>To inquire about or join VIP areas listed below, follow links for teams below actively recruiting now! </p>
-        </p>
+          <p style = {style.font}>To inquire about or join VIP areas listed below, follow links for teams below actively recruiting now! </p>
+        </Card>
         <h1 style = {{textAlign:'center', color:Secondary}}>Announcements</h1>
         <AnnouncementList pageLength = {2} />
         <hr style={style.hr}/>
