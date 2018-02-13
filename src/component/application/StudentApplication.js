86d69e52c07a0eef75564e11ebd2e19f9622e8d9
Sampaleen <spaleen@udel.edu>
2018-02-13 18:01:47 -0500

    Application Refactor start

---
+++ src/component/application/StudentApplication.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class StudentApplication extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  
+
+  render(){
+    return(<p>student application works!</p>);
+  }
+}
+
+export default StudentApplication;
